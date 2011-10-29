8e54c547c8801283626f337384056576d6753d1b
King Wilder <info@kingwilder.com>
2011-10-29 09:52:51 -0700

    Trying to commit again...

--- MvcApplication5/Properties/AssemblyInfo.cs
+++ MvcApplication5/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MvcInstaller.MVC2")]
-[assembly: AssemblyDescription("ASP.NET MVC Installer - the easy way to install your database-driven MVC application on your production server.")]
+[assembly: AssemblyTitle("MvcApplication5")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Gizmo Beach")]
-[assembly: AssemblyProduct("MvcInstaller - for ASP.NET MVC 2")]
-[assembly: AssemblyCopyright("Copyright © Gizmo Beach 2011")]
-[assembly: AssemblyTrademark("Gizmo Beach")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MvcApplication5")]
+[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f5a54ab1-ad28-44d6-b55b-aea448ca4f00")]
+[assembly: Guid("0e17a828-f552-4745-986e-34930732ea35")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.138.34")]
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
